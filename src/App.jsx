3af6b8eaee719247fdc6b5ea9cf81e455ddbf02a
gkekals <gkekals12@kbu.ac.kr>
2025-08-19 15:40:24 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import './App.css';
-import { useReducer, useRef } from 'react';
+import { useReducer, useRef, createContext, useEffect } from 'react';
 import { Routes, Route, useNavigate, data } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 import Notfound from './pages/Notfound';
-import { getEmotionImage } from './util/getEmotionImage';
-import Header from './components/Header';
-import Button from './components/Button';
 
 
 const mockData = [
   {
     id: 1,
-    createDate: new Date().getTime(),
+    createDate: new Date("2025-08-17").getTime(),
     emotionId: 1,
     content: "1번 일기 내용"
   },
   {
     id: 2,
-    createDate: new Date().getTime(),
+    createDate: new Date("2025-07-05").getTime(),
     emotionId: 2,
     content: "2번 일기 내용"
+  },
+  {
+    id: 3,
+    createDate: new Date("2024-12-05").getTime(),
+    emotionId: 4,
+    content: "3번 일기 내용"
   }
 ]
 
 function reducer(state, action) {
   switch (action.type) {
+    case "INIT":
+      return action.data
     case "CREATE":
       return [action.data, ...state]
     case "UPDATE":
@@ -36,19 +41,28 @@ function reducer(state, action) {
           action.data
           : item
       )
-      case "DELETE":
-        return state.filter(
-          (item)=>String(item.id)!==String(action.id)
-        )
+    case "DELETE":
+      return state.filter(
+        (item) => String(item.id) !== String(action.id)
+      )
     default:
       return state
   }
 }
 
+export const DiaryStateContext = createContext()
+export const DiaryDispatchContext = createContext()
 function App() {
 
   const [data, dispatch] = useReducer(reducer, mockData)
-  const idRef = useRef(3)
+  const idRef = useRef(4)
+
+  useEffect(() => {
+    dispatch({
+      type: "INIT",
+      data: mockData
+    })
+  }, [])
 
   const onCreate = (createDate, emotionId, content) => {
 
@@ -73,36 +87,27 @@ function App() {
       }
     })
   }
-const onDelete=(id)=>{
-  dispatch({
-    type:"DELETE",
-    id
-  })
-}
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id
+    })
+  }
   return (
-    <div>
-      <Header
-        leftChild={<Button text="left" />}
-        title="header title"
-        rightChild={<Button text="right" />}
-      />
 
-      <button onClick={() => onCreate(
-        new Date().getTime(), 1, "hello"
-      )}>일기 추가하기</button>
-      <button onClick={() => onUpdate(
-        1,new Date().getTime(), 3, "수정된 내용"
-      )}>일기 수정하기</button>
-      <button onClick={() => onDelete(1)}>일기 삭제하기</button>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/edit/" element={<Edit />} />
-        <Route path="/diary" element={<Diary />} />
-        <Route path='*' element={<Notfound />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit/" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path='*' element={<Notfound />} />
+        </Routes>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+
   )
 }
 
